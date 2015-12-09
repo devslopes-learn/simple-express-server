1d28e0007872540342a346a2cea64ea10f3f0ed6
spentak <mprice@verisagellc.com>
2015-12-08 20:26:27 -0700

    cors

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+//Allow all requests from all domains & localhost
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -12,8 +19,14 @@ var animals = [
     "penguin"
 ];
 
+var ingredients = [{"id":1,"text":"ham"},{"id":2,"text":"cheese"},{"id":3,"text":"potatoes"},{"id":4,"text":"honey"}];
+
 app.get('/animals', function(req, res) {
     res.send(animals);
+});
+
+app.get('/ingredients', function(req, res) {
+    res.send(ingredients);
 });
 
 app.listen(6060);
