c5b7e3412b446f7759e65f8708250268d32790af
spentak <mprice@verisagellc.com>
2015-12-11 21:15:55 -0700

    added content type accept and sample for post

--- server.js
+++ server.js
@@ -5,23 +5,44 @@ var app = express();
 //Allow all requests from all domains & localhost
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET");
   next();
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var animals = [
-    "puppy",
-    "kitten",
-    "ferocious bear",
-    "penguin"
+var ingredients = [
+    {
+        "id": "234kjw",
+        "text": "Eggs"
+    },
+    {
+        "id": "as82w",
+        "text": "Milk"
+    },
+    {
+        "id": "234sk1",
+        "text": "Bacon"
+    },
+    {
+        "id": "ppo3j3",
+        "text": "Frog Legs"
+    }
 ];
 
 
-app.get('/animals', function(req, res) {
-    res.send(animals);
+app.get('/ingredients', function(req, res) {
+    console.log("GET From SERVER");
+    res.send(ingredients);
 });
 
-app.listen(6060);
+app.post('/ingredients', function(req, res) {
+    var ingredient = req.body;
+    console.log(req.body);
+    ingredients.push(ingredient);
+    res.status(200).send("Successfully posted ingredient");
+});
+
+app.listen(6069);
